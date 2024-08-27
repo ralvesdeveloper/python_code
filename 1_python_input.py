5da597cd503cde8ff505ad7bb29ed64e50b51ce5
Rogerio Alves <roger.fladzigan@gmail.com>
2024-08-27 19:15:11 -0300

    ralves1

---
+++ 1_python_input.py
@@ -0,0 +1,4 @@
+val1=int(input("Digite o valor inicial: "))
+if val1 == int:
+    print("Valor")
+print(x)
---
+++ 2_python_methods.py
@@ -0,0 +1,8 @@
+#example of method on object
+#object
+nome=str('rogerio')
+
+#method upper to manipulate the object
+print('==========================')
+print(nome.upper())
+print('==========================')
